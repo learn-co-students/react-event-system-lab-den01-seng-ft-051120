54baa99e4570a49d521732b10890908de7b2e28c
hawleywoo <alhawley21@gmail.com>
2020-07-24 21:49:59 -0600

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default function Keypad(){
+
+    const handleKey = () => {
+        console.log('Entering password...')
+    }
+
+    return(
+        <input onKeyUp={handleKey} type='password' />
+    )
+}
